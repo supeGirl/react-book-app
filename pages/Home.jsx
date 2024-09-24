faab2d1e5f36fd5afcbc581ddee231eddb42d7d0
supaGirl <kerenvasserman.bm@gmail.com>
2024-09-24 21:05:08 +0300

    Change the view in Home-page(add meme and style)

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,9 +2,10 @@
 
 export function Home() {
     return (
-        <section>
+        <section className="home-page main-layout">
             <h2>Welcome to my book-app</h2>
          {/*    <UserPreview /> */}
+            <img src="../assets/img/bookmemecover.webp" alt="bookcover" />
         </section>
     )
 }
